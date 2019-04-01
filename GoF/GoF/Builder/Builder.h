efcdf6acfe8a9649baf7d4b0ecd3b85396d8e5ea
GBW <GBW@GBW-PC>
2019-04-01 22:58:45 +0800

    .

---
+++ GoF/GoF/Builder/Builder.h
@@ -0,0 +1,26 @@
+#pragma once
+class House {
+
+public:
+	// 稳定构建算法
+	void init() 
+	{
+
+		this->BuildPart1();
+
+		for (int i = 0; i < 4; i++) {
+			this->BuildPart2();
+		}
+
+		this->BuildPart3();
+	}
+	// 各个部分的子对象
+	virtual void BuildPart1() = 0;
+	virtual void BuildPart2() = 0;
+	virtual void BuildPart3() = 0;
+
+};class StoneHouse : public House {
+	virtual void BuildPart1() {};
+	virtual void BuildPart2() {};
+	virtual void BuildPart3() {};
+};
--- GoF/GoF/原型模式/Main.cpp
+++ GoF/GoF/原型模式/Main.cpp
@@ -14,10 +14,16 @@ public:
 		proto->split();
 	}
 };
-void main()
+void mains()
 {
 	Prototype_A* proto_A = new Prototype_A();
 	Main* ma = new Main(proto_A);
 	ma->exec();
 
 }
+/*
+	原型模式就是用原型实例指定创建对象的种类，并且通过复制这些原型创建新的对象。
+	Prototype：抽象原型类。声明克隆自身的接口。
+	ConcretePrototype：具体原型类。实现克隆的具体操作。
+	Client：客户类。让一个原型克隆自身，从而获得一个新的对象。
+*/
